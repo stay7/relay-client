432166e6b05b633fdda3ac329669f96a863e9b39
stay7 <codeLatte20@gmail.com>
2021-07-09 20:11:40 +0900

    Implement open mail app

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:get/get.dart';
 import 'package:relay/components/setting-menu-item.dart';
 import 'package:relay/components/setting-menu.dart';
-import 'package:relay/config/color.dart';
 import 'package:relay/controller/config_controller.dart';
 import 'package:relay/header/app_header.dart';
 import 'package:relay/provider/info_provider.dart';
 
 class SettingPage extends StatelessWidget {
   final configController = Get.find<ConfigController>();
+
+  openMail() async {
+    final Email email = Email(
+      subject: 'Email subject',
+      body: 'Email body',
+      recipients: ['example@example.com'],
+      isHTML: false,
+    );
+
+    try {
+      await FlutterEmailSender.send(email);
+    } catch (error) {
+      //TODO: show error dialog
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +41,7 @@ class SettingPage extends StatelessWidget {
         child: Obx(
           () => Column(
             children: [
-              SettingMenu(icon: Icon(Icons.settings_outlined), title: '시스템'),
+              SettingMenu(icon: Icon(Icons.settings_outlined), title: '일반'),
               SettingMenuItem(
                 title: '연속 추가',
                 isSwitch: true,
@@ -35,16 +50,16 @@ class SettingPage extends StatelessWidget {
                   configController.switchKeepAddWord();
                 },
               ),
+              SettingMenu(icon: Icon(Icons.settings_outlined), title: '시스템'),
               SettingMenuItem(
-                  onPress: () {},
-                  title: '버전 정보',
-                  content: '${InfoProvider.appVersion}'),
-              SettingMenu(icon: Icon(Icons.settings_outlined), title: '시스템'),
-              SettingMenuItem(onPress: () {}, title: '버전 정보', content: '1.2.3'),
-              SettingMenuItem(onPress: () {}, title: '버전 정보', content: '1.2.3'),
-              SettingMenu(icon: Icon(Icons.settings_outlined), title: '시스템'),
-              SettingMenuItem(onPress: () {}, title: '버전 정보', content: '1.2.3'),
-              SettingMenuItem(onPress: () {}, title: '버전 정보', content: '1.2.3'),
+                title: '문의',
+                onPress: openMail(),
+              ),
+              SettingMenuItem(
+                title: '버전 정보',
+                onPress: () {},
+                content: '${InfoProvider.appVersion}',
+              ),
             ],
           ),
         ),
